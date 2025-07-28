3c77b4fc6d26eed1b881fe999b9a5f74f64d2030
Lucas Godina <lucasgodina@gmail.com>
2025-07-27 21:48:42 -0300

    comments app

--- articles/models.py
+++ articles/models.py
@@ -20,8 +20,7 @@ class Article(models.Model):
 
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article,
-                                on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
     comment = models.CharField(max_length=140)
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
@@ -30,7 +29,6 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.comment
-    
+
     def get_absolute_url(self):
         return reverse("article_list")
-    
